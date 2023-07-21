314e67883b9c0a1732259f023714078c18dcf2fc
Andrii <andrii.khomik@gmail.com>
2023-07-21 14:04:15 +0300

    add menu

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Hello AI Saas (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
